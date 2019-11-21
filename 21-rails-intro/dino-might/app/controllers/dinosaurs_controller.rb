a9ce6b723e18f02142a063ff7da5c764416cfc6f
Trevor Jameson <tjameson15@gmail.com>
2019-11-21 11:44:19 -0500

    renamed rails lecture

--- 21-rails-intro/dino-might/app/controllers/dinosaurs_controller.rb
+++ 21-rails-intro/dino-might/app/controllers/dinosaurs_controller.rb
@@ -1,6 +1,7 @@
 class DinosaursController < ApplicationController
     def index
         @dinosaurs = Dinosaur.all
-        erb :'dinos2'
+        # Rendering index.erb is handled implicitly. No need for line below.
+        render :index
     end
 end
