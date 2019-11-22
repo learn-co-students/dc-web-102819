ccbe124992e14a899984ebf21090fae3c6ae5afe
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2019-11-22 14:51:16 -0500

    post lecture 22

---
+++ 22-rails-app/snack-tracking/app/controllers/snacks_controller.rb
@@ -0,0 +1,52 @@
+class SnacksController < ApplicationController
+
+  before_action :find_snack, only: [:show, :edit, :update, :destroy]
+  
+  def home
+  end
+
+  def index
+    @snacks = Snack.all 
+    render :index
+  end
+
+  def my_super_fun_page
+  end
+
+  def show
+  end
+
+  def new
+    @snack = Snack.new
+  end
+
+  def create
+    snack = Snack.create(snack_strong_params)
+    redirect_to snack_path(snack)
+  end
+
+  def edit
+  end
+
+  def update
+    @snack.update(snack_strong_params)
+    redirect_to (snack_path(@snack))
+  end
+
+  def destroy
+    @snack.delete
+    redirect_to snacks_path
+  end
+
+  def find_snack
+    @snack = Snack.find(params[:id])
+  end
+
+  private
+
+  def snack_strong_params
+    params.require(:snack).permit(:name, :calories, :deliciousness)
+
+  end
+
+end
---
+++ 22-rails-app/snack-tracking/db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Snack.destroy_all
+
+Snack.create(name: "Nachos", calories: 600, deliciousness: 2)
+Snack.create(name: "Twinkies", calories: 400, deliciousness: 4)
+Snack.create(name: "Ice Cream Cake", calories: 500, deliciousness: 9)
