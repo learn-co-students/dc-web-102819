84809d5cd884800f56142991b537828dbe77fc43
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2019-12-12 15:59:10 -0500

    post lecture

--- 35-single-page-applications/script.js
+++ 35-single-page-applications/script.js
@@ -1,64 +1,84 @@
-// document.addEventListener("DOMContentLoaded", function(){
-//   console.log('DOM loaded')
-// });
-
 document.addEventListener("DOMContentLoaded", function(){
-  gottaFetchEmAll()
-  getForm().addEventListener("submit", submitHandler)
+    gottaFetchEmAll()
+    getForm().addEventListener("submit", processNewPokemonForm)
+
 });
 
 function gottaFetchEmAll() {
     fetch("http://localhost:3000/pokemon")
-        .then( response => response.json() )
-        .then( pokemonArray => {
+        .then(response => response.json())
+        .then(pokemonArray => {
             pokemonArray.forEach(pokemon => renderPokemon(pokemon))
         })
+
 }
 
-function renderPokemon(pokemon){
+function renderPokemon(pokemon) {
+
     let container = document.getElementById("pokemon-container")
     let pokemonCard = document.createElement("div")
-    pokemonCard.id = `pokemon-${pokemon.id}`
     pokemonCard.classList.add("card")
-    let header = document.createElement("h3")
+    pokemonCard.id = `pokemon-${pokemon.id}`
+    let header = document.createElement('h3')
     header.innerText = pokemon.name
     pokemonCard.appendChild(header)
+    container.appendChild(pokemonCard)
     let image = document.createElement("img")
     image.src = pokemon.sprite
     pokemonCard.appendChild(image)
-    container.appendChild(pokemonCard)
     pokemonCard.addEventListener("click", deletePokemon)
+
 }
 
 function getForm() {
-    return document.querySelector("form")
+    return document.getElementById("new-pokemon-form")
 }
 
-function submitHandler(e) {
-    e.preventDefault()
-    postPokemon()
-    getForm().reset()
-
+function processNewPokemonForm(event) {
+    event.preventDefault()
+    let newName = document.querySelector("#name-input").value
+    let newSprite = document.querySelector("#sprite-input").value
+    postPokemonData(newName, newSprite)
 }
 
-function postPokemon(){
-    let data = {"name": document.querySelector("#name-input").value ,
-                "sprite": getForm()[1].value} // two ways to do the same thing
-    console.log(data)
-    fetch('http://localhost:3000/pokemon', {
+function postPokemonData(name, sprite_url) {
+    let newPokemonData = {"name": name,
+                          "sprite": sprite_url}
+    json_string = JSON.stringify(newPokemonData)
+    fetch("http://localhost:3000/pokemon", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
-        },
-        body: JSON.stringify(data)
+        }, 
+        body: json_string
     }).then(res => res.json())
     .then(data => renderPokemon(data))
+    getForm().reset()
 }
 
-function deletePokemon(event){
-    let id = event.currentTarget.id.split("-")[1]
+function deletePokemon(event) {
+    let card = event.currentTarget
+    let id = card.id.split("-")[1]
     fetch(`http://localhost:3000/pokemon/${id}`, {
         method: "DELETE"
-    })
-    event.currentTarget.remove()
+    }).then(res => card.remove())
+    .catch(error => alert(error))
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
