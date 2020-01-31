9d4269f5db5a90312c7e6f54833b67e0a6ec5bca
canikwe <c.anikwe@gmail.com>
2020-01-31 15:33:19 -0500

    fix typo

--- 61-deployment/blogger/backend/config/initializers/cors.rb
+++ 61-deployment/blogger/backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://polar-waters-40193.herokuapp.com/'
+    origins 'https://polar-waters-40193.herokuapp.com'
 
     resource '*',
       headers: :any,
